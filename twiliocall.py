b1e677b33043e898292eae13f29400afc27e0aaf
Sachin lobo <30945603+sachinlobo11@users.noreply.github.com>
2020-11-19 07:23:18 +0530

    Add files via upload

---
+++ twiliocall.py
@@ -0,0 +1,31 @@
+from twilio.rest import Client
+from flask import Flask, redirect, url_for,render_template
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+@app.route("/oncall")
+def click():
+    account_sid = 'ACe97016ec1e8b3ad790572a389c0cda64'
+    auth_token = '03db640dc6a105d6fd4c850162e58407'
+    client = Client(account_sid, auth_token)
+
+    call = client.calls.create(
+                            url='http://demo.twilio.com/docs/voice.xml',
+                            to='+918747917217',
+                            from_='+918746878795'
+                        )
+
+    print(call.sid)
+    return render_template('in.html')
+    
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=False,host='192.168.0.102')
